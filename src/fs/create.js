21c67464e1a058b240450665fc0a01f7c265dcbb
iozefavichus <iozefavichus@gmail.com>
2024-01-26 21:59:53 +0200

    fix: fix create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,11 @@
+import { writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fresh.txt');
+
 const content = 'I am fresh and young';
 
 const create = async () => {
