73ba1816f0d851b2c2d47ee639b61742ff4bd924
iozefavichus <iozefavichus@gmail.com>
2024-01-29 20:43:52 +0200

    fix: fix write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { pipeline } from 'stream/promises';
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(filePath, { flags: 'a' });
+
+  await pipeline(process.stdin, writableStream);
 };
 
 await write();
