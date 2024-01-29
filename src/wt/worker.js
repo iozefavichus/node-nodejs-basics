573c67f9333bca53b6ab18a6ca4847223d1d4b7a
iozefavichus <iozefavichus@gmail.com>
2024-01-29 21:41:11 +0200

    fix: fix worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,11 +1,10 @@
-
-const n = parseInt(process.argv[2], 10);
-console.log(n);
+import { parentPort, workerData } from 'worker_threads';
 
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(workerData);
+	parentPort.postMessage(result);
 };
 
 sendResult();
