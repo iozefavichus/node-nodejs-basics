48a2eddf72b7438a2839dbbb51adcc3a77074003
iozefavichus <iozefavichus@gmail.com>
2024-01-29 20:42:17 +0200

    fix:fix read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(filePath);
+
+  await pipeline(readableStream, process.stdout);
 };
 
 await read();
