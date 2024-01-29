617d0047cecce84066b863d1731a89ae6a0afa4e
iozefavichus <iozefavichus@gmail.com>
2024-01-29 19:59:52 +0200

    fix: fix: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+const prefix = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const partsArray = args.reduce((result, value, index) => {
+    if (index % 2 === 0) {
+      if (value.startsWith(prefix)) {
+        const propName = value.substring(2);
+        const valueName = args[index + 1];
+        result.push(`${propName} is ${valueName}`);
+      }
+    }
+    return result;
+  }, []);
+
+  const output = partsArray.join(', ');
+  console.log(output);
 };
 
 parseArgs();
