1e17924982a6b152f46f4ac7ffe35c709b62ea53
iozefavichus <iozefavichus@gmail.com>
2024-01-29 21:03:38 +0200

    fix: fix calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  try {
+    const content = await readFile(filePath);
+    hash.update(content);
+    console.log(hash.digest('hex'));
+  } catch (error) {
+		throw new Error('Error! Operation is failed');
+  }
 };
 
 await calculateHash();
